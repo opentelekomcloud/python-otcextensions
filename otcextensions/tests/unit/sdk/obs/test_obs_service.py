fffda7012bef1bfde623ab34230d2ecbd5ec7f5d
Artem Goncharov <artem.goncharov@gmail.com>
2018-02-20 16:24:09 +0100

    switch to tests.unit.base

--- otcextensions/tests/unit/sdk/obs/test_obs_service.py
+++ otcextensions/tests/unit/sdk/obs/test_obs_service.py
@@ -10,12 +10,12 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import testtools
+from openstack.tests.unit import base
 
 from otcextensions.sdk.obs import obs_service
 
 
-class TestObsService(testtools.TestCase):
+class TestObsService(base.TestCase)):
 
     def test_service(self):
         sot = obs_service.ObsService()
