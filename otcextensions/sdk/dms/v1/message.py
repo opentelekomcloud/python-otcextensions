1702ecab05d1506349c976ecab9ff21904360925
hodigy <hodigy@gmail.com>
2018-04-26 23:06:42 +0200

    pop8 error corrections

--- otcextensions/sdk/dms/v1/message.py
+++ otcextensions/sdk/dms/v1/message.py
@@ -13,7 +13,7 @@ from openstack import resource
 from openstack import _log
 
 from otcextensions.sdk.dms.v1 import _base
-_logger= _log.setup_logging('openstack')
+_logger = _log.setup_logging('openstack')
 
 
 class Message(_base.Resource):
