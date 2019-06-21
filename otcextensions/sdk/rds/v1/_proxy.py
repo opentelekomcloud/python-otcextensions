97c98a8aec3737a2f9ea459220dfec3eaeb8ae11
Artem Goncharov <artem.goncharov@gmail.com>
2019-06-21 11:28:12 +0200

    Fix #38

--- otcextensions/sdk/rds/v1/_proxy.py
+++ otcextensions/sdk/rds/v1/_proxy.py
@@ -9,7 +9,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from openstack import _log
 from openstack import exceptions
 
 from otcextensions.sdk import sdk_proxy
@@ -18,8 +17,6 @@ from otcextensions.sdk.rds.v1 import configuration as _configuration
 from otcextensions.sdk.rds.v1 import datastore as _datastore
 from otcextensions.sdk.rds.v1 import flavor as _flavor
 from otcextensions.sdk.rds.v1 import instance as _instance
-
-_logger = _log.setup_logging('openstack')
 
 
 class Proxy(sdk_proxy.Proxy):
