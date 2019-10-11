5ba785ea001912ced0881b20b5bbfc552227542a
Artem Goncharov <artem.goncharov@gmail.com>
2019-10-11 14:48:26 +0200

    hide lcoation of backup

--- otcextensions/osclient/rds/v3/backup.py
+++ otcextensions/osclient/rds/v3/backup.py
@@ -28,7 +28,9 @@ _formatters = {
 
 def _get_columns(item):
     column_map = {}
-    return sdk_utils.get_osc_show_columns_for_sdk_resource(item, column_map)
+    hidden = ['location']
+    return sdk_utils.get_osc_show_columns_for_sdk_resource(
+        item, column_map, hidden)
 
 
 class ListBackup(command.Lister):
