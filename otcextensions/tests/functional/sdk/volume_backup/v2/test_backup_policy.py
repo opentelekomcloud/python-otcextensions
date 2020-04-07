0eb52cc0272d84f2dd6a62ded1267b363ef91a50
Artem Goncharov <Artem.goncharov@gmail.com>
2020-04-07 11:02:01 +0200

    sync with upstream

--- otcextensions/tests/functional/sdk/volume_backup/v2/test_backup_policy.py
+++ otcextensions/tests/functional/sdk/volume_backup/v2/test_backup_policy.py
@@ -34,9 +34,8 @@ class TestBackupPolicy(TestVbs):
     policy = None
     volume = None
 
-    @classmethod
-    def setUpClass(cls):
-        super(TestBackupPolicy, cls).setUpClass()
+    def setUp(cls):
+        super(TestBackupPolicy, cls).setUp()
         # for volume in cls.conn.block_store.volumes(limit=1):
         #     cls.volume = volume
         #     break
@@ -45,8 +44,7 @@ class TestBackupPolicy(TestVbs):
         # create backup policy
         cls.policy = create_backup_policy(cls.client, cls.BACKUP_POLICY_NAME)
 
-    @classmethod
-    def tearDownClass(cls):
+    def tearDown(cls):
         #: delete backup policy
         if cls.policy and cls.volume:
             cls.conn.volume_backup.unlink_resources_of_policy(cls.policy,
@@ -79,53 +77,3 @@ class TestBackupPolicy(TestVbs):
     def test_execute_policy(self):
         policy = self.client.enable_policy(self.policy)
         self.client.execute_policy(policy)
-
-    # def test_link(self):
-    #     VOLUME_NAME = self.getUniqueString()
-    #     self.volume = self.conn.block_storage.create_volume(
-    #                 name=VOLUME_NAME,
-    #                 size=1)
-    #     self.client.link_resources_to_policy(self.policy, [self.volume.id])
-#
-#     def get_current_policy(self):
-#         policies = list(self.conn.volume_backup.backup_policies())
-#         for policy in policies:
-#             if policy.id == self.policy.id:
-#                 return policy
-#
-#     def test_1_list_backup_policies(self):
-#         policies = list(self.conn.volume_backup.backup_policies())
-#         self.assertIn(self.policy.name, [p.name for p in policies])
-#
-#     def test_2_update_backup_policy(self):
-#         updated = {
-#             "scheduled_policy": {
-#                 "frequency": 5,
-#                 "start_time": "01:00"
-#             }
-#         }
-#         self.conn.volume_backup.update_backup_policy(self.policy, **updated)
-#         policy = self.get_current_policy()
-#         self.assertEqual(5, policy.scheduled_policy.frequency)
-#         self.assertEqual("01:00", policy.scheduled_policy.start_time)
-#         self.policy = policy
-#
-#     def test_3_bind_and_execute(self):
-#         if self.policy.scheduled_policy.status == "OFF":
-#             self.conn.volume_backup.enable_policy(self.policy)
-#
-#         self.conn.volume_backup.link_resources_to_policy(self.policy,
-#                                                          [self.volume.id])
-#         self.conn.volume_backup.execute_policy(self.policy)
-#
-#     def test_4_enable_disable_policy(self):
-#         if self.policy.scheduled_policy.status == "ON":
-#             self.conn.volume_backup.disable_policy(self.policy)
-#             policy = self.get_current_policy()
-#             self.assertEqual("OFF", policy.scheduled_policy.status)
-#         self.conn.volume_backup.enable_policy(self.policy)
-#         self.assertEqual("ON", self.policy.scheduled_policy.status)
-#
-#     def test_5_list_tasks(self):
-#         tasks = list(self.conn.volume_backup.tasks(self.policy.id))
-#         print(tasks)
