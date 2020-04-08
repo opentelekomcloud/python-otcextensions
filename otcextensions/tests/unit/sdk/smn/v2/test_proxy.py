a07d6df6219a767869e4be1912bc5d089fb8c9ca
Artem Goncharov <Artem.goncharov@gmail.com>
2020-04-08 11:11:51 +0200

    fix missed stuff in version renaming

--- otcextensions/tests/unit/sdk/smn/v2/test_proxy.py
+++ otcextensions/tests/unit/sdk/smn/v2/test_proxy.py
@@ -10,7 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from otcextensions.sdk.smn.v1 import _proxy
+from otcextensions.sdk.smn.v2 import _proxy
 
 from openstack.tests.unit import test_proxy_base
 
