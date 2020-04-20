2a0a95298a5e234b2f3963dce561370c87f39d9f
Artem Goncharov <Artem.goncharov@gmail.com>
2020-04-20 16:20:24 +0200

    drop wrongly distributed header

--- otcextensions/tests/unit/osclient/test_base.py
+++ otcextensions/tests/unit/osclient/test_base.py
@@ -1,6 +1,3 @@
-#   Copyright 2012-2013 OpenStack Foundation
-#   Copyright 2013 Nebula Inc.
-#
 #   Licensed under the Apache License, Version 2.0 (the "License"); you may
 #   not use this file except in compliance with the License. You may obtain
 #   a copy of the License at
