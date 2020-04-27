3aa0ffb60353dbb835033af9a9aa7d6695bd07b2
Vladimir Hasko <vladimir.hasko@t-systems.com>
2020-04-27 11:43:56 +0000

    fixes based on Travis findings

--- otcextensions/tests/functional/sdk/dms/v1/test_instance.py
+++ otcextensions/tests/functional/sdk/dms/v1/test_instance.py
@@ -13,16 +13,8 @@ import openstack
 from openstack import exceptions
 from openstack import resource
 
-# from openstack import utils
-from openstack.tests.functional.network.v2 import test_network
-from openstack.network.v2 import security_group
-from openstack.network.v2 import network
-from openstack.network.v2 import router
-from openstack.network.v2 import subnet
-
 
 from otcextensions.tests.functional import base
-import time
 import uuid
 import random
 
@@ -45,24 +37,21 @@ class TestInstance(base.BaseFunctionalTest):
     def setUp(self):
         super(TestInstance, self).setUp()
         self._initialize_network()
-        openstack.enable_logging(debug=True, http_debug=True)
-        products=list(self.conn.dms.products())
-        products_ids=[product.product_id for product in products]
-        product_id=random.choice(products_ids)
-        availability_zone_names=[
+        products = list(self.conn.dms.products())
+        products_ids = [product.product_id for product in products]
+        product_id = random.choice(products_ids)
+        availability_zone_names = [
             product.availability_zones for product in products
             if product_id == product.product_id
-            ]
-        availability_zone_name=random.choice(sum(availability_zone_names, []))
-        availability_zones=[
-            availability_zone.id
-            for availability_zone in self.conn.dms.availability_zones()
-            ]
-        availability_zone_id=[
+        ]
+        availability_zone_name = random.choice(
+            sum(availability_zone_names, [])
+        )
+        availability_zone_id = [
             availability_zone.id
             for availability_zone in self.conn.dms.availability_zones()
             if availability_zone_name == availability_zone.code
-            ]
+        ]
         try:
             self.instance = self.conn.dms.create_instance(
                 name=self.INSTANCE_NAME,
@@ -86,9 +75,9 @@ class TestInstance(base.BaseFunctionalTest):
             wait=900)
         self.assertIn('instance_id', self.instance)
         self.instances.append(self.instance)
+
     def tearDown(self):
         super(TestInstance, self).tearDown()
-        openstack.enable_logging(debug=True, http_debug=True)
         try:
             for instance in self.instances:
                 if instance.id:
@@ -112,12 +101,10 @@ class TestInstance(base.BaseFunctionalTest):
             self.assertIsNotNone(instance)
 
     def _initialize_network(self):
-        openstack.enable_logging(debug=True, http_debug=True)
         self.cidr = '192.168.0.0/16'
         self.ipv4 = 4
 
         sg = self.conn.network.create_security_group(name=self.SG_NAME)
-        assert isinstance(sg, security_group.SecurityGroup)
         self.assertEqual(self.SG_NAME, sg.name)
         TestInstance.SG_ID = sg.id
 
@@ -130,7 +117,7 @@ class TestInstance(base.BaseFunctionalTest):
             ip_version=self.ipv4,
             network_id=TestInstance.NET_ID,
             cidr=self.cidr
-            )
+        )
         self.assertEqual(self.SUBNET_NAME, subnet.name)
         TestInstance.SUBNET_ID = subnet.id
 
@@ -141,14 +128,14 @@ class TestInstance(base.BaseFunctionalTest):
                                          subnet_id=TestInstance.SUBNET_ID)
         self.assertEqual(interface['subnet_id'], TestInstance.SUBNET_ID)
         self.assertIn('port_id', interface)
-        #add CIDR to Router for compatibility
+        # add CIDR to Router for compatibility
         service = self.conn.identity.find_service('vpc')
-        endpoints=list(self.conn.identity.endpoints(service_id=service.id))
+        endpoints = list(self.conn.identity.endpoints(service_id=service.id))
         endpoint_url = endpoints[0].url
-        endpoint = endpoint_url[:endpoint_url.rfind("/")+1];
+        endpoint = endpoint_url[:endpoint_url.rfind("/") + 1]
         cidr = self.conn.compute.put(
-            endpoint + self.conn.session.get_project_id() + '/vpcs/' +
-            self.ROUTER_ID, data='{"vpc": {"cidr": "192.168.0.0/16"}}',
+            endpoint + self.conn.session.get_project_id() + '/vpcs/'
+            + self.ROUTER_ID, data='{"vpc": {"cidr": "192.168.0.0/16"}}',
             headers={'content-type': 'application/json'})
         self.assertIn('cidr', cidr.json()['vpc'])
         self.assertIn('192.168.0.0/16', cidr.json()['vpc']['cidr'])
