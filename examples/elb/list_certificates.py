fe217a697eae0cd67c9cb2fe8f3ea0c7c56986f6
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-11-02 13:32:38 +0300

    added list example

--- examples/elb/list_certificates.py
+++ examples/elb/list_certificates.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
@@ -9,14 +10,13 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+"""
+List all Recordsets of a DNS Zone
+"""
+import openstack
 
-from otcextensions.sdk.elb.v2 import _proxy as _proxy_v2
-from openstack import service_description
+openstack.enable_logging(True)
+conn = openstack.connect(cloud='otc')
 
-
-class ElbService(service_description.ServiceDescription):
-    """The identity service."""
-
-    supported_versions = {
-        '2': _proxy_v2.Proxy,
-    }
+for certificate in conn.elb.cerificates():
+    print(certificate)
