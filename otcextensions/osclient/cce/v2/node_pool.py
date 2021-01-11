bbdda9b3bc6d2fe8bed7e495d25372669ac49d64
tischrei <tino.schreiber@t-systems.com>
2021-01-11 10:34:48 +0000

    git review corrections

--- otcextensions/osclient/cce/v2/node_pool.py
+++ otcextensions/osclient/cce/v2/node_pool.py
@@ -10,7 +10,7 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 #
-'''CCE Cluster Nodes v2 action implementations'''
+'''CCE Node Pool CLI implementation'''
 import logging
 
 from osc_lib import utils
