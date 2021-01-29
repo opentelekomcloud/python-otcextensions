b0e82c480937448b8230f2ba33e56c5c6b7053d0
tischrei <tino.schreiber@t-systems.com>
2021-01-29 12:33:21 +0000

    CBR changes

---
+++ otcextensions/sdk/cbr/v3/_proxy.py
@@ -0,0 +1,96 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from openstack import proxy
+
+from otcextensions.sdk.cbr.v3 import backup as _backup
+
+
+class Proxy(proxy.Proxy):
+
+    skip_discovery = True
+
+    # ======== Backup ========
+    def backups(self, **query):
+        """Retrieve a generator of Backups
+
+        :param dict query: Optional query parameters to be sent to limit the
+            resources being returned.
+            * `checkpoint_id`: Restore point ID
+            * `dec`: Dedicated Cloud
+            * `end_time`: Time when the backup ends
+            * `enterprise_project_id`: Enterprise project ID
+            * `image_type`: Backup type: backup or replication
+            * `limit`: Number of records displayed per page
+            * `marker`: ID of the last record displayed
+            * `member_status`: Backup sharing status
+            * `name`: Backup name
+            * `offset`: Offset value
+            * `parent_id`: Parent backup ID
+            * `resource_az`: AZ-based filtering
+            * `resource_id`: Resource ID
+            * `resource_name`: Resource name
+            * `resource_type`: Resource Type
+            * `sort`: sorting
+            * `start_time`: Time when the backup starts
+            * `status`: Status
+            * `vault_id`: Vault ID
+
+        :returns: A generator of backup
+            :class:`~otcextensions.sdk.cbr.v3.backup.Backup` instances
+        """
+        return self._list(_backup.Backup, **query)
+
+    def get_backup(self, backup):
+        """Get the backup by UUID.
+
+        :param backup: key id or an instance of
+            :class:`~otcextensions.sdk.cbr.v3.backup.backup`
+
+        :returns: instance of
+            :class:`~otcextensions.sdk.cbr.v3.backup.backup`
+        """
+        return self._get(
+            _backup.Backup, backup
+        )
+    
+    def find_backup(self, name_or_id, ignore_missing=True):
+        """Find a single CBR backup by name or id
+
+        :param name_or_id: The name or ID of a backup
+        :param bool ignore_missing: When set to ``False``
+            :class:`~openstack.exceptions.ResourceNotFound` will be raised
+            when the group does not exist.
+            When set to ``True``, no exception will be set when attempting
+            to delete a nonexistent group.
+
+        :returns: ``None``
+        """
+        return self._find(
+            _backup.Backup, name_or_id,
+            ignore_missing=ignore_missing,
+        )
+
+    def delete_backup(self, backup, ignore_missing=True):
+        """Delete a single CBR backup.
+
+        :param backup: The value can be the ID of a backup
+             or a :class:`~otcextensions.sdk.cbr.v3.backup.backup`
+             instance.
+        :param bool ignore_missing: When set to ``False``
+            :class:`~openstack.exceptions.ResourceNotFound` will be raised when
+            the group does not exist.
+            When set to ``True``, no exception will be set when attempting to
+            delete a nonexistent backup.
+        """
+        return self._delete(
+            _backup.Backup, backup, ignore_missing=ignore_missing,
+        )
