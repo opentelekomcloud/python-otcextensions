ca728661e7db4cbdf0f9a24fb4b88a1ee179392a
tischrei <tino.schreiber@t-systems.com>
2021-02-02 12:50:53 +0000

    unittests done for restore

--- otcextensions/tests/unit/sdk/cbr/v3/test_backup.py
+++ otcextensions/tests/unit/sdk/cbr/v3/test_backup.py
@@ -1,14 +1,15 @@
-# Licensed under the Apache License, Version 2.0 (the 'License'); you may
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+
 import uuid
 from keystoneauth1 import adapter
 import mock
@@ -18,40 +19,41 @@ from otcextensions.sdk.cbr.v3 import backup as _backup
 
 
 EXAMPLE = {
-    'provider_id' : uuid.uuid4(),
-    'checkpoint_id' : uuid.uuid4(),
-    'updated_at' : '2020-02-21T07:07:25.113761',
-    'vault_id' : uuid.uuid4(),
-    'id' : uuid.uuid4(),
-    'resource_az' : 'eu-de-01',
-    'image_type' : 'backup',
-    'resource_id' : uuid.uuid4(),
-    'resource_size' : 40,
-    'children' : [ ],
-    'extend_info' : {
-        'auto_trigger' : True,
-        'supported_restore_mode' : 'backup',
-        'contain_system_disk' : True,
-        'support_lld' : True,
-        'architecture' : 'x86_64',
-        'system_disk' : False,
-        'app_consistency' : {
-            'app_consistency_error_code' : '0',
-            'app_consistency_status' : '0',
-            'app_consistency_error_message' : '',
-            'app_consistency' : '0'
+    'provider_id': uuid.uuid4(),
+    'checkpoint_id': uuid.uuid4(),
+    'updated_at': '2020-02-21T07:07:25.113761',
+    'vault_id': uuid.uuid4(),
+    'id': uuid.uuid4(),
+    'resource_az': 'eu-de-01',
+    'image_type': 'backup',
+    'resource_id': uuid.uuid4(),
+    'resource_size': 40,
+    'children': [],
+    'extend_info': {
+        'auto_trigger': True,
+        'supported_restore_mode': 'backup',
+        'contain_system_disk': True,
+        'support_lld': True,
+        'architecture': 'x86_64',
+        'system_disk': False,
+        'app_consistency': {
+            'error_code': '0',
+            'error_status': '0',
+            'error_message': '',
+            'app_consistency': '0'
         }
     },
-    'project_id' : uuid.uuid4(),
-    'status' : 'available',
-    'resource_name' : 'test001-02',
-    'description' : '',
-    'expired_at' : '2020-05-21T07:00:54.060493',
-    'replication_records' : [ ],
-    'name' : 'autobk_b629',
-    'created_at' : '2020-02-21T07:00:54.065135',
-    'resource_type' : 'OS::Nova::Server'
+    'project_id': uuid.uuid4(),
+    'status': 'available',
+    'resource_name': 'test001-02',
+    'description': '',
+    'expired_at': '2020-05-21T07:00:54.060493',
+    'replication_records': [],
+    'name': 'autobk_b629',
+    'created_at': '2020-02-21T07:00:54.065135',
+    'resource_type': 'OS::Nova::Server'
 }
+
 
 class TestBackup(base.TestCase):
 
@@ -77,6 +79,29 @@ class TestBackup(base.TestCase):
         self.assertTrue(sot.allow_fetch)
         self.assertFalse(sot.allow_commit)
         self.assertTrue(sot.allow_delete)
+        self.assertDictEqual({
+            'checkpoint_id': 'checkpoint_id',
+            'dec': 'dec',
+            'end_time': 'end_time',
+            'enterprise_project_id': 'enterprise_project_id',
+            'image_type': 'image_type',
+            'limit': 'limit',
+            'marker': 'marker',
+            'member_status': 'member_status',
+            'name': 'name',
+            'offset': 'offset',
+            'own_type': 'own_type',
+            'parent_id': 'parent_id',
+            'resource_az': 'resource_az',
+            'resource_id': 'resource_id',
+            'resource_name': 'resource_name',
+            'resource_type': 'resource_type',
+            'sort': 'sort',
+            'start_time': 'start_time',
+            'status': 'status',
+            'used_percent': 'used_percent',
+            'vault_id': 'vault_id'
+        }, sot._query_mapping._mapping)
 
     def test_make_it(self):
         test_backup = _backup.Backup(**EXAMPLE)
@@ -137,3 +162,29 @@ class TestBackup(base.TestCase):
         self.assertEqual(
             EXAMPLE['resource_type'],
             test_backup.resource_type)
+        self.assertTrue(
+            test_backup.extend_info.auto_trigger)
+        self.assertEqual(
+            EXAMPLE['extend_info']['supported_restore_mode'],
+            test_backup.extend_info.supported_restore_mode)
+        self.assertTrue(
+            test_backup.extend_info.contain_system_disk)
+        self.assertTrue(
+            test_backup.extend_info.support_lld)
+        self.assertEqual(
+            EXAMPLE['extend_info']['architecture'],
+            test_backup.extend_info.architecture)
+        self.assertFalse(
+            test_backup.extend_info.system_disk)
+        self.assertEqual(
+            EXAMPLE['extend_info']['app_consistency']['app_consistency'],
+            test_backup.extend_info.app_consistency.app_consistency)
+        self.assertEqual(
+            EXAMPLE['extend_info']['app_consistency']['error_code'],
+            test_backup.extend_info.app_consistency.error_code)
+        self.assertEqual(
+            EXAMPLE['extend_info']['app_consistency']['error_message'],
+            test_backup.extend_info.app_consistency.error_message)
+        self.assertEqual(
+            EXAMPLE['extend_info']['app_consistency']['error_status'],
+            test_backup.extend_info.app_consistency.error_status)
