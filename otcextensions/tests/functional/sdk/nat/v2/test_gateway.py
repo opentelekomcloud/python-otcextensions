66a92c8dbd3ce94b2c106ec3c0695fa407f1465c
Polina-Gubina <Polina.Gubina@t-systems.com>
2021-06-07 19:09:25 +0300

    Add tests for gateway

--- otcextensions/tests/functional/sdk/nat/v2/test_gateway.py
+++ otcextensions/tests/functional/sdk/nat/v2/test_gateway.py
@@ -9,7 +9,11 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-import openstack
+
+from openstack import proxy
+from openstack import resource
+from openstack import utils
+
 from otcextensions.tests.functional import base
 from datetime import datetime
 
@@ -17,10 +21,37 @@ _logger = openstack._log.setup_logging('openstack')
 
 
 class TestGateway(base.BaseFunctionalTest):
-    CURR_TIME = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+
+    UPDATE_NAME = "updated-gateway"
 
     def setUp(self):
         super(TestGateway, self).setUp()
+        self.GATEWAY_NAME = self.getUniqueString('gateway')
+        self.VPC_NAME = self.getUniqueString('vpc')
+        self.spec = 1
+        self.NETWORK_NAME = self.getUniqueString('network')
+        self.network = self.conn.network.create_network(name=self.NETWORK_NAME)
+        self.vpc = self.conn.network.create_router(name=self.VPC_NAME)
+        gateway = self.conn.nat.create_gateway(name=self.GATEWAY_NAME,
+                                    internal_network_id=self.network['id'],
+                                    router_id=self.vpc['id'],
+                                    spec=self.spec)
+        self.ID = gateway['id']
+
+    def test_find_gateway(self):
+
+        gw = self.conn.nat.find_gateway(self.GATEWAY_NAME)
+        self.assertEqual(self.ID, gw.id)
+
+    def test_get(self):
+        gw = self.conn.nat.get_gateway(self.ID)
+        self.assertEqual(self.GATEWAY_NAME, gw.name)
+        self.assertEqual(self.ID, gw.id)
+
+    def test_update(self):
+        gw = self.conn.network.update_gateway(self.ID,
+                                                 name=self.UPDATE_NAME)
+        self.assertEqual(self.UPDATE_NAME, gw.name)
 
     def test_list(self):
         gateways = list(self.conn.nat.gateways())
@@ -41,3 +72,11 @@ class TestGateway(base.BaseFunctionalTest):
         }
         gateways = list(self.conn.nat.gateways(**attrs))
         self.assertGreaterEqual(len(gateways), 0)
+
+    def tearDown(self):
+        gw = self.conn.nat.delete_gateway(self.ID, ignore_missing=True)
+        self.assertIsNone(gw)
+
+        self.conn.network.delete_network(name=self.NETWORK_NAME)
+        self.conn.network.delete_router(name=self.VPC_NAME)
+        super(TestGateway, self).tearDown()
