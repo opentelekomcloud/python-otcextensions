5636e48789e7d463160af9596c950c6e902f7fdb
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2021-06-24 14:34:39 +0300

    rollback boto

--- otcextensions/tests/functional/sdk/cts/v1/test_tracker.py
+++ otcextensions/tests/functional/sdk/cts/v1/test_tracker.py
@@ -13,61 +13,36 @@ import uuid
 
 import openstack
 import openstack.exceptions as ex
-from boto3.session import Session
+
 from otcextensions.tests.functional.sdk.cts import TestCts
 
 _logger = openstack._log.setup_logging('openstack')
 
 
 class TestTraces(TestCts):
-    s3_endpoint = 'https://obs.eu-de.otc.t-systems.com'
     uuid_v4 = uuid.uuid4().hex[:8]
     tracker_name = 'system'
     tracker_prefix = f'{uuid_v4}-cts-sdk-'
     bucket_name = uuid_v4 + '-sdk-test'
-    obs = None
-    bucket = None
 
     def setUp(self):
         super(TestTraces, self).setUp()
-        if hasattr(self.conn, 'get_ak_sk'):
-            (ak, sk) = self.conn.get_ak_sk(self.conn)
-        if not (ak and sk):
-            self.log.error('Cannot obtain AK/SK from config')
-            return None
-        session = Session(aws_access_key_id=ak,
-                          aws_secret_access_key=sk)
-
-        self.obs = session.resource('s3', endpoint_url=self.s3_endpoint)
-        self._create_bucket()
-        if self.bucket:
-            try:
-                self.tracker = self.client.create_tracker(
-                    bucket_name=self.bucket_name,
-                    file_prefix_name=self.tracker_prefix,
-                )
-            except (ex.BadRequestException, ex.HttpException):
-                self.tracker = self.client.get_tracker(self.tracker_name)
+        try:
+            self.tracker = self.client.create_tracker(
+                bucket_name=self.bucket_name,
+                file_prefix_name=self.tracker_prefix,
+            )
+        except (ex.BadRequestException, ex.HttpException):
+            self.tracker = self.client.get_tracker(self.tracker_name)
 
     def tearDown(self):
         super(TestTraces, self).tearDown()
-        if self.bucket:
-            self._delete_bucket()
         try:
             if self.tracker:
                 self.client.delete_tracker(self.tracker)
         except openstack.exceptions.SDKException as e:
             _logger.warning('Got exception during clearing resources %s'
                             % e.message)
-
-    def _create_bucket(self):
-        self.bucket = self.obs.create_bucket(Bucket=self.bucket_name)
-        self.bucket.Acl().put(ACL='public-read-write')
-        self.assertIsNotNone(self.bucket)
-
-    def _delete_bucket(self):
-        res = self.obs.delete_bucket(Bucket=self.bucket_name)
-        self.assertIsNone(res)
 
     def test_list_traces(self):
         traces = self.client.traces()
