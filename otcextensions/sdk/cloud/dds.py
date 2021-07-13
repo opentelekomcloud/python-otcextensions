c6bdfbc6ef6433b64dd5ef41db7bcd1995fbd2e8
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2021-07-13 11:53:47 +0300

    explicit params

--- otcextensions/sdk/cloud/dds.py
+++ otcextensions/sdk/cloud/dds.py
@@ -14,168 +14,134 @@ from openstack import exceptions
 
 class DdsMixin:
     def create_dds_instance(self,
-                            name,
+                            name: str,
+                            router,
+                            network,
+                            security_group,
+                            flavors: list,
+                            password: str,
                             region='eu-de',
                             availability_zone='eu-de-01',
                             datastore_type='DDS-Community',
                             datastore_storage_engine='wiredTiger',
+                            datastore_version='3.2',
                             mode='ReplicaSet',
-                            **kwargs):
-        """Create DDS instance with all the checks
+                            disk_encryption_id: str = None,
+                            backup_timeframe: str = None,
+                            backup_keepdays: str = None,
+                            ssl_option: str = None
+                            ):
+        """
 
-        :param str availability_zone: dict(type=str, default='eu-de-01'),
-        :param str backup_timeframe: dict(type=str),
-        :param str backup_keepdays: dict(type=str),
-        :param str datastore_type: dict(type=str, default='DDS-Community'),
-        :param str datastore_version: dict(type=str),
-        :param str datastore_storage_engine:
-            dict(type=str, default='wiredTiger'),
-        :param str disk_encryption_id: dict(type=str),
-        :param str flavors: dict(required=True, type=list, elements=dict),
-        :param str mode: dict(choices=['Sharding', 'ReplicaSet'],
+        :param name: instance name, dict(required=True, type=str)
+        :param router: router name or id, dict(type=str)
+        :param network: network name or id, dict(type=str)
+        :param security_group: sg name or id, dict(type=str)
+        :param flavors: list of flavors,
+            dict(type=list, elements=dict)
+        :param password: password, dict(type=str, no_log=True)
+        :param region: dict(type=str, default='eu-de')
+        :param availability_zone: dict(type=str, default='eu-de-01'),
+        :param datastore_type: dict(type=str, default='DDS-Community')
+        :param datastore_storage_engine:
+            dict(type=str, default='wiredTiger')
+        :param datastore_version: dict(type=str, choices=['3.2', '3.4']),
+        :param mode: dict(choices=['Sharding', 'ReplicaSet'],
             default='ReplicaSet')
-        :param str name: dict(required=True, type=str),
-        :param str network: dict(type=str),
-        :param str password: dict(type=str, no_log=True),
-        :param str region:
-            dict(type=str, default='eu-de'),
-        :param str router: dict(type=str),
-        :param str security_group: dict(type=str),
-        :param str ssl_option: dict(type=str),
+        :param disk_encryption_id: dict(type=str)
+        :param backup_timeframe: dict(type=str)
+        :param backup_keepdays: dict(type=str)
+        :param ssl_option: dict(type=str, choices=['0', '1'])
 
         :returns: The results of instance creation
         :rtype: :class:`~otcextensions.sdk.dds.v3.instance.Instance`
         """
 
-        datastore_version = kwargs.get('datastore_version')
-        router = kwargs.get('router')
-        network = kwargs.get('network')
-        security_group = kwargs.get('security_group')
-        password = kwargs.get('password')
-        disk_encryption_id = kwargs.get('disk_encryption_id')
-        flavors = kwargs.get('flavors')
-        backup_timeframe = kwargs.get('backup_timeframe')
-        backup_keepdays = kwargs.get('backup_keepdays')
-        ssl_option = kwargs.get('ssl_option')
-
         attrs = {}
         attrs['name'] = name
+        attrs['region'] = region
+        attrs['availability_zone'] = availability_zone
+        attrs['password'] = password
 
-        if datastore_type:
-            datastore = {
-                'type': datastore_type,
-                'version': datastore_version,
-                'storage_engine': datastore_storage_engine
-            }
-            attrs['datastore'] = datastore
-        elif datastore_type or datastore_version or datastore_storage_engine:
-            raise exceptions.SDKException(
-                '`datastore_type` and `datastore_version`'
-                ' and `datastore_storage_engine` must be passed together'
-            )
+        datastore = {
+            'type': datastore_type,
+            'version': datastore_version,
+            'storage_engine': datastore_storage_engine
+        }
+        attrs['datastore'] = datastore
 
-        if region:
-            attrs['region'] = region
+        router_obj = self.network.find_router(router, ignore_missing=False)
+        attrs['vpc_id'] = router_obj.id
 
-        if availability_zone:
-            attrs['availability_zone'] = availability_zone
+        network_obj = self.network.find_network(network, ignore_missing=False)
+        attrs['subnet_id'] = network_obj.id
 
-        if router:
-            router_obj = self.network.find_router(router, ignore_missing=False)
-            attrs['vpc_id'] = router_obj.id
-        else:
-            raise exceptions.SDKException(
-                '`router` is mandatory parameter'
-            )
-
-        if network:
-            network_obj = self.network.find_network(
-                network, ignore_missing=False)
-            attrs['subnet_id'] = network_obj.id
-        else:
-            raise exceptions.SDKException(
-                '`network` is mandatory parameter'
-            )
-
-        if security_group:
-            security_group_obj = self.network.find_security_group(
-                security_group, ignore_missing=False)
-            attrs['security_group_id'] = security_group_obj.id
-        else:
-            raise exceptions.SDKException(
-                '`security_group` is mandatory parameter'
-            )
-
-        if password:
-            attrs['password'] = password
-        else:
-            raise exceptions.SDKException(
-                '`password` is mandatory parameter'
-            )
+        security_group_obj = self.network.find_security_group(
+            security_group, ignore_missing=False)
+        attrs['security_group_id'] = security_group_obj.id
 
         if disk_encryption_id:
             attrs['disk_encryption_id'] = disk_encryption_id
 
-        if mode:
-            if mode not in ['Sharding', 'ReplicaSet']:
-                raise exceptions.SDKException(
-                    '`Sharding` or `ReplicaSet` are supported values'
-                )
-            attrs['mode'] = mode
+        if mode not in ['Sharding', 'ReplicaSet']:
+            raise exceptions.SDKException(
+                '`Sharding` or `ReplicaSet` are supported values'
+            )
+        attrs['mode'] = mode
 
         flavors_ref = list(self.dds.flavors(
             region=region,
             engine_name=datastore_type)
         )
         flavors_specs = [flavor.spec_code for flavor in flavors_ref]
-        if flavors:
-            for flavor in flavors:
-                if flavor['type'] in ['mongos', 'shard'] \
-                        and flavor['num'] not in range(2, 16):
+
+        for flavor in flavors:
+            if flavor['type'] in ['mongos', 'shard'] \
+                    and flavor['num'] not in range(2, 16):
+                raise exceptions.SDKException(
+                    '`num` value must be in ranges from 2 to 16 '
+                    'for mongos and shard'
+                )
+            if flavor['type'] in ['config', 'replica'] \
+                    and flavor['num'] != 1:
+                raise exceptions.SDKException(
+                    '`num` value must be 1 '
+                    'for config and replica'
+                )
+            if flavor['type'] == 'mongos':
+                if all(k in flavor for k in ('storage', 'size')):
                     raise exceptions.SDKException(
-                        '`num` value must be in ranges from 2 to 16 '
-                        'for mongos and shard'
+                        '`storage` and `size` parameters'
+                        ' is invalid for the mongos nodes'
                     )
-                if flavor['type'] in ['config', 'replica'] \
-                        and flavor['num'] != 1:
+            if 'size' in flavor:
+                if flavor['type'] == 'replica' \
+                        and not (10 <= flavor['size'] <= 2000):
                     raise exceptions.SDKException(
-                        '`num` value must be 1 '
-                        'for config and replica'
+                        '`size` value for `replica` must be'
+                        ' between 10 and 2000 GB.'
                     )
-                if flavor['type'] == 'mongos':
-                    if all(k in flavor for k in ('storage', 'size')):
-                        raise exceptions.SDKException(
-                            '`storage` and `size` parameters'
-                            ' is invalid for the mongos nodes'
-                        )
-                if 'size' in flavor:
-                    if flavor['type'] == 'replica' \
-                            and not (10 <= flavor['size'] <= 2000):
-                        raise exceptions.SDKException(
-                            '`size` value for `replica` must be'
-                            ' between 10 and 2000 GB.'
-                        )
-                    elif flavor['type'] == 'config' \
-                            and flavor['size'] != 20:
-                        raise exceptions.SDKException(
-                            '`size` value for `config` must be 20 GB.'
-                        )
-                    elif not (10 <= flavor['size'] <= 1000):
-                        raise exceptions.SDKException(
-                            '`size` value for `shard` must be'
-                            ' between 10 and 1000 GB.'
-                        )
-                if flavor['spec_code'] not in flavors_specs:
+                elif flavor['type'] == 'config' \
+                        and flavor['size'] != 20:
                     raise exceptions.SDKException(
-                        '`spec_code` not valid'
+                        '`size` value for `config` must be 20 GB.'
                     )
-            attrs['flavor'] = flavors
+                elif not (10 <= flavor['size'] <= 1000):
+                    raise exceptions.SDKException(
+                        '`size` value for `shard` must be'
+                        ' between 10 and 1000 GB.'
+                    )
+            if flavor['spec_code'] not in flavors_specs:
+                raise exceptions.SDKException(
+                    '`spec_code` not valid'
+                )
+        attrs['flavor'] = flavors
 
         if backup_keepdays and backup_timeframe:
-            backup_attrs = {}
-            backup_attrs['keep_days'] = backup_keepdays
-            backup_attrs['start_time'] = backup_timeframe
-            attrs['backup_strategy'] = backup_attrs
+            attrs['backup_strategy'] = {
+                'keep_days': backup_keepdays,
+                'start_time': backup_timeframe
+            }
         elif backup_keepdays or backup_timeframe:
             raise exceptions.SDKException(
                 '`backup_keepdays` and `backup_timeframe` must be passed'
