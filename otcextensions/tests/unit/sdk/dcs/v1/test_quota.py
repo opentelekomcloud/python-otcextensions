80f4fccc1089450c06f7a48f08d476baaf1b7e37
Irina Pereiaslavskaia <irina.pereyaslavskaya@gmail.com>
2021-12-22 16:55:10 +0300

    Distributed Cache Service Quota resource with tests.

---
+++ otcextensions/tests/unit/sdk/dcs/v1/test_quota.py
@@ -0,0 +1,28 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from openstack.tests.unit import base
+
+from otcextensions.sdk.dcs.v1 import quota
+
+
+class TestQuota(base.TestCase):
+
+    def test_basic(self):
+        sot = quota.Quota()
+        self.assertEqual('quotas.resources', sot.resources_key)
+        self.assertEqual('/quota', sot.base_path)
+        self.assertTrue(sot.allow_list)
+        self.assertFalse(sot.allow_create)
+        self.assertFalse(sot.allow_fetch)
+        self.assertFalse(sot.allow_commit)
+        self.assertFalse(sot.allow_delete)
