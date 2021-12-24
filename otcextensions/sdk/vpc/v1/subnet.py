747044f30b91dd5aa762c58f28ceeac3fc7fb426
Anton Kachurin <anton.kachurin@t-systems.com>
2021-12-24 16:11:07 +0300

    Add `vpc.v1.subnet` resource (#256)
    Add `vpc.v1.subnet` resource
    
    Add subnet methods to vpc.v1.proxy
    Add acceptance tests for vpc.v1.subnet
    Add vpc.v1.vpc and vpc.v1.subnet documentation
    
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: Anton Sidelnikov <None>
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>
    Reviewed-by: None <None>

---
+++ otcextensions/sdk/vpc/v1/subnet.py
@@ -0,0 +1,66 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from openstack import resource
+
+
+class ExtraDHCPOpt(resource.Resource):
+    #: Specifies the NTP server address configured for the subnet.
+    opt_value = resource.Body('opt_value')
+    #: Specifies the NTP server address name configured for the subnet.
+    #: Currently, the value can only be set to ntp.
+    opt_name = resource.Body('opt_name', default='ntp')
+
+
+class Subnet(resource.Resource):
+    resource_key = 'subnet'
+    resources_key = 'subnets'
+    base_path = '/v1/%(project_id)s/subnets'
+
+    # capabilities
+    allow_create = True
+    allow_fetch = True
+    allow_commit = True
+    allow_delete = True
+    allow_list = True
+
+    # Properties
+    project_id = resource.URI('project_id')
+    description = resource.Body('description')
+    #: Specifies the subnet CIDR block.
+    cidr = resource.Body('cidr')
+    #: Specifies the gateway of the subnet.
+    gateway_ip = resource.Body('gateway_ip')
+    #: Specifies whether DHCP is enabled for the subnet.
+    dhcp_enable = resource.Body('dhcp_enable')
+    #: Specifies the IP address of DNS server 1 on the subnet.
+    primary_dns = resource.Body('primary_dns')
+    #: Specifies the IP address of DNS server 2 on the subnet.
+    secondary_dns = resource.Body('secondary_dns')
+    #: Specifies the DNS server address list of a subnet.
+    #: This field is required if use more than two DNS servers.
+    dns_list = resource.Body('dnsList', type=list, list_type=str)
+    #: Specifies the AZ to which the subnet belongs
+    availability_zone = resource.Body('availability_zone')
+    #: Specifies the ID of the VPC to which the subnet belongs.
+    vpc_id = resource.Body('vpc_id')
+    #: Specifies the NTP server address configured for the subnet.
+    extra_dhcp_opts = resource.Body('extra_dhcp_opts', type=list,
+                                    list_type=ExtraDHCPOpt)
+
+    status = resource.Body('status')
+    neutron_network_id = resource.Body('neutron_network_id')
+    neutron_subnet_id = resource.Body('neutron_subnet_id')
+
+
+def vpc_subnet_base_path(vpc_id):
+    """Special case of subnet resource path"""
+    return f'/v1/%(project_id)s/vpcs/{vpc_id}/subnets'
---
+++ otcextensions/tests/functional/sdk/vpc/v1/test_subnet.py
@@ -0,0 +1,131 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+import uuid
+
+from openstack import _log
+from openstack import resource
+
+from otcextensions.sdk.vpc.v1 import vpc
+from otcextensions.tests.functional import base
+
+_logger = _log.setup_logging('openstack')
+
+
+class TestService(base.BaseFunctionalTest):
+    _vpc: vpc.Vpc
+    _seed: str
+
+    @property
+    def seed(self):
+        if not hasattr(self, '_seed') or self._seed is None:
+            self._seed = uuid.uuid4().hex[:8]
+        return self._seed
+
+    def setUp(self):
+        super().setUp()
+
+        self.client = self.conn.vpc
+        attrs = {
+            'name': 'test-vpc-' + self.seed,
+            'cidr': '192.168.0.0/16'
+        }
+        self.vpc = self.conn.vpc.create_vpc(**attrs)
+        self.addCleanup(self.conn.vpc.delete_vpc, self.vpc)
+
+    def test_create_subnet(self):
+        cidr = self.vpc.cidr
+        gateway, _ = cidr.split("/")
+        gateway = gateway[:-2] + ".1"  # .0 -> .1
+
+        attrs = {
+            'vpc_id': self.vpc.id,
+            'name': 'test-subnet-' + self.seed,
+            'cidr': cidr,
+            'gateway_ip': gateway,
+            'dns_list': [
+                "100.125.4.25",
+                "100.125.129.199",
+            ],
+        }
+        subnet = self.conn.vpc.create_subnet(**attrs)
+        self.assertIsNotNone(subnet.id)
+        self.addCleanup(self._delete_subnet, subnet)
+
+        self.assertIsNotNone(subnet.status)
+        self.assertIsNotNone(subnet.neutron_subnet_id)
+        self.assertIsNotNone(subnet.neutron_network_id)
+
+    def test_get_subnet(self):
+        subnet = self._create_subnet()
+        found = self.conn.vpc.get_subnet(subnet.id)
+
+        self.assertEqual(found, subnet)
+
+    def test_find_subnet(self):
+        subnet = self._create_subnet()
+        found = self.conn.vpc.find_subnet(subnet.name)
+
+        self.assertEqual(found, subnet)
+
+    def test_update_subnet(self):
+        subnet = self._create_subnet()
+
+        new_attrs = {
+            'name': 'test-updated-' + self.seed,
+            'dns_list': [
+                "100.125.4.25",
+                "8.8.8.8",
+            ],
+        }
+        updated = self.conn.vpc.update_subnet(subnet, **new_attrs)
+        self.assertEqual(updated.name, new_attrs['name'])
+        self.assertEqual(updated.dns_list, new_attrs['dns_list'])
+
+    def test_delete_subnet(self):
+        subnet = self._create_subnet(False)
+        self.conn.vpc.delete_subnet(subnet, ignore_missing=False)
+        resource.wait_for_delete(self.conn.vpc, subnet, 2, 120)
+
+    def test_list_subnets(self):
+        subnet = self._create_subnet()
+
+        subnets = list(self.conn.vpc.subnets())
+        self.assertGreaterEqual(len(subnets), 1)
+
+        self.assertIn(subnet, subnets)
+
+    def _create_subnet(self, remove=True):
+        cidr: str = self.vpc.cidr
+        gateway, _ = cidr.split("/")
+        gateway = gateway[:-2] + ".1"  # .0 -> .1
+
+        attrs = {
+            'vpc_id': self.vpc.id,
+            'name': 'test-subnet-' + self.seed,
+            'cidr': cidr,
+            'gateway_ip': gateway,
+            'dns_list': [
+                "100.125.4.25",
+                "100.125.129.199",
+            ],
+        }
+        subnet = self.conn.vpc.create_subnet(**attrs)
+        resource.wait_for_status(self.conn.vpc, subnet, "ACTIVE", None, 2, 20)
+
+        if remove:
+            self.addCleanup(self._delete_subnet, subnet)
+        return subnet
+
+    def _delete_subnet(self, subnet):
+        resource.wait_for_status(self.conn.vpc, subnet, "ACTIVE", None, 2, 20)
+        self.conn.vpc.delete_subnet(subnet, ignore_missing=False)
+        resource.wait_for_delete(self.conn.vpc, subnet, 2, 60)
