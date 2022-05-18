e009892f1aac178240a1439f7e5367435856fa59
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2022-05-18 15:08:48 +0300

    abort method

--- otcextensions/sdk/obs/v1/_proxy.py
+++ otcextensions/sdk/obs/v1/_proxy.py
@@ -476,16 +476,15 @@ class Proxy(sdk_proxy.Proxy):
                 url, headers, upload_id)
         except Exception:
             try:
-                segment_prefix = endpoint.split('/')[-1]
                 self.log.debug(
-                    "Failed to upload large object manifest for %s. "
-                    "Removing segment uploads.", segment_prefix)
-                self._delete_autocreated_image_objects(
-                    segment_prefix=segment_prefix)
+                    "Failed to upload large object for %s. "
+                    "Aborting uploads.", upload_id)
+                self._abort_multipart_upload(endpoint=url,
+                    upload_id=upload_id)
             except Exception:
                 self.log.exception(
                     "Failed to cleanup image objects for %s:",
-                    segment_prefix)
+                    upload_id)
             raise
 
     def parts(self, endpoint, upload_id, requests_auth):
@@ -658,22 +657,13 @@ class Proxy(sdk_proxy.Proxy):
         res.delete_metadata(self, keys)
         return res
 
-    def _delete_autocreated_image_objects(
-            self, container=None, segment_prefix=None
+    def _abort_multipart_upload(
+            self, endpoint, upload_id
     ):
-        """Delete all objects autocreated for image uploads.
-        This method should generally not be needed, as shade should clean up
-        the objects it uses for object-based image creation. If something goes
-        wrong and it is found that there are leaked objects, this method can be
-        used to delete any objects that shade has created on the user's behalf
-        in service of image uploads.
-        :param str container: Name of the container. Defaults to 'images'.
-        :param str segment_prefix: Prefix for the image segment names to
-            delete. If not given, all image upload segments present are
-            deleted.
+        """Aborts all multipart upload objects.
+        :param endpoint: Container endpoint
+        :param upload_id: ID of the multipart upload to be aborted.
+        :return:
         """
-        deleted = False
-        for obj in self.objects(container, prefix=segment_prefix):
-            self.delete_object(obj, ignore_missing=True)
-            deleted = True
-        return deleted
+        url = f'{endpoint}?uploadId={upload_id}'
+        return self.delete(url, requests_auth=self._get_req_auth(endpoint))
