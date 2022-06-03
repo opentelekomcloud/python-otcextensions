548d17057bb990d4c718b0f9657294238e1fe973
Irina Pereiaslavskaia <irina.pereyaslavskaya@gmail.com>
2022-06-03 22:39:24 +0300

    Change tests.

--- otcextensions/tests/functional/sdk/dcaas/v2/test_endpoint_group.py
+++ otcextensions/tests/functional/sdk/dcaas/v2/test_endpoint_group.py
@@ -19,13 +19,36 @@ _logger = openstack._log.setup_logging('openstack')
 
 class TestDirectConnectEndpointGroup(base.BaseDCTest):
 
+    UUID = base.BaseDCTest.UUID
+    CIDR = base.BaseDCTest.CIDR
+    TYPE = "cidr"
+    EG_NAME = "test-eg-" + UUID
+
     def setUp(self):
         super(TestDirectConnectEndpointGroup, self).setUp()
         self.dcaas = self.conn.dcaas
 
     def tearDown(self):
-        super(TestDirectConnectEndpointGroup, self).tearDown()
+        try:
+            self._delete_endpoint_group()
+        finally:
+            super(TestDirectConnectEndpointGroup, self).tearDown()
+
+    def _create_endpoint_group(self):
+        project_id = self.conn.current_project_id()
+        endpoints = [self.CIDR]
+        self.endpoint_group = self.dcaas.create_endpoint_group(
+            project_id=project_id,
+            name=self.EG_NAME,
+            endpoints=endpoints,
+            type=self.TYPE
+        )
+
+    def _delete_endpoint_group(self):
+        if self.endpoint_group:
+            self.dcaas.delete_endpoint_group(self.endpoint_group.id)
 
     def test_list_endpoint_group(self):
+        self._create_endpoint_group()
         endpoint_groups = list(self.dcaas.endpoint_groups())
-        self.assertGreaterEqual(len(endpoint_groups), 0)
+        self.assertEqual(len(endpoint_groups), 1)
