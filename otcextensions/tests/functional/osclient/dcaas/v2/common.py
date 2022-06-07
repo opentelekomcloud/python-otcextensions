7e1ebc3654b5c6476c9b43ec983776ce326e5459
Irina Pereiaslavskaia <irina.pereyaslavskaya@gmail.com>
2022-06-08 02:47:25 +0300

    New functional tests and releasenotes.

---
+++ otcextensions/tests/functional/osclient/dcaas/v2/common.py
@@ -0,0 +1,124 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+import json
+import uuid
+
+from openstackclient.tests.functional import base
+
+
+class DcaasTestCase(base.TestCase):
+    """Common functional test bits for DCAAS commands"""
+
+    UUID = uuid.uuid4().hex[:9]
+    NETWORK_NAME = "test-dc-network-" + UUID
+    SUBNET_NAME = "test-dc-subnet-" + UUID
+    ROUTER_NAME = "test-dc-router-" + UUID
+    IP_VERSION = 4
+    CIDR = "192.168.0.0/16"
+    EP_GROUP_NAME = "test-dc-eg-" + UUID
+    EP_GROUP_TYPE = "cidr"
+
+    def setUp(self):
+        super(DcaasTestCase, self).setUp()
+        self.create_test_infra()
+        self.EP_GROUP_ID = self.create_endpoint_group()
+
+    def tearDown(self):
+        try:
+            if self.EP_GROUP_ID:
+                self.delete_endpoint_group()
+        finally:
+            self.delete_test_infra()
+            super(DcaasTestCase, self).tearDown()
+
+    def _get_project_id(self):
+        json_output = json.loads(self.openstack(
+            'token issue -f json'
+        ))
+        return json_output['project_id']
+
+    def _create_network(self):
+        json.loads(self.openstack(
+            'network create -f json ' + self.NETWORK_NAME
+        ))
+
+    def _delete_network(self):
+        json.loads(self.openstack('network delete ' + self.NETWORK_NAME))
+
+    def _create_subnet(self):
+        json.loads(self.openstack(
+            'subnet create {subnet} -f json'
+            '--network {network} '
+            '--subnet-range {subnet_range} '.format(
+                subnet=self.SUBNET_NAME,
+                network=self.NETWORK_NAME,
+                subnet_range=self.CIDR)
+        ))
+
+    def _delete_subnet(self):
+        json.loads(self.openstack('subnet delete ' + self.SUBNET_NAME))
+
+    def _create_router(self):
+        json.loads(self.openstack(
+            'router create -f json ' + self.ROUTER_NAME
+        ))
+        json.loads(self.openstack(
+            'router add subnet {router} {subnet} '.format(
+                router=self.ROUTER_NAME,subnet=self.SUBNET_NAME
+            )
+        ))
+
+    def _router_remove_subnet(self):
+        json.loads(self.openstack(
+            'router remove subnet {router} {subnet} '.format(
+                router=self.ROUTER_NAME,
+                subnet=self.SUBNET_NAME
+            )
+        ))
+
+    def _delete_router(self):
+        json.loads(self.openstack('router delete ' + self.ROUTER_NAME))
+
+    def create_test_infra(self):
+        self._create_network()
+        self._create_subnet()
+        self._create_router()
+
+    def delete_test_infra(self):
+        self._router_remove_subnet()
+        self._delete_subnet()
+        self._delete_network()
+        self._delete_router()
+
+    def create_endpoint_group(self):
+        project_id = self._get_project_id()
+        endpoints = [self.CIDR]
+        json_output = json.loads(self.openstack(
+            'dcaas endpoint group create '
+            '{project_id} '
+            '{endpoints} '
+            '{type} '
+            '--name {name} -f json'.format(
+                name=self.EP_GROUP_NAME,
+                project_id=project_id,
+                endpoints=endpoints,
+                type=self.EP_GROUP_TYPE
+            )
+        ))
+        return json_output['id']
+
+    def delete_endpoint_group(self):
+        json.loads(self.openstack(
+            'dcaas endpoint group delete ' + self.EP_GROUP_NAME
+        ))
---
+++ otcextensions/tests/functional/osclient/dcaas/v2/test_endpoint_group.py
@@ -0,0 +1,49 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import json
+import uuid
+
+from otcextensions.tests.functional.osclient.dcaas.v2 import common
+
+
+class TestEndpointGroup(common.DcaasTestCase):
+
+    def setUp(self):
+        super(TestEndpointGroup, self).setUp()
+
+    def test_list_endpoint_groups(self):
+        json_output = json.loads(self.openstack(
+            'dcaas endpoint group list -f json '
+        ))
+        self.assertIsNotNone(json_output)
+
+    def test_find_endpoint_group(self):
+        json_output = json.loads(self.openstack(
+            'dcaas endpoint group show {id} -f json'.format(
+                id=self.EP_GROUP_ID
+            )
+        ))
+        self.assertIsNotNone(json_output)
+        self.assertEqual(json_output['name'], self.EP_GROUP_NAME)
+
+    def test_update_endpoint_group(self):
+        ep_group_name = self.EP_GROUP_NAME + '-updated'
+        json_output = json.loads(self.openstack(
+            'dcaas endpoint group update '
+            '{id} --name {name} -f json'.format(
+                id=self.EP_GROUP_ID,
+                name=ep_group_name
+            )
+        ))
+        self.assertEqual(json_output['id'], self.EP_GROUP_ID)
+        self.assertEqual(json_output['name'], ep_group_name)
