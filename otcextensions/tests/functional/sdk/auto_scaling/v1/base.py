090158256a297659423585092c0ac6859bdbf79e
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2022-08-26 19:03:05 +0300

    reworked as tests

--- otcextensions/tests/functional/sdk/auto_scaling/v1/base.py
+++ otcextensions/tests/functional/sdk/auto_scaling/v1/base.py
@@ -9,43 +9,62 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
+from openstack import utils
 import os
 import uuid
-
+from openstack import exceptions
 import fixtures
-
 from openstack import _log
+
 from otcextensions.tests.functional import base
 
 _logger = _log.setup_logging('openstack')
 
 
-class BaseASTest(base.BaseFunctionalTest):
+class TestAs(base.BaseFunctionalTest):
+    TIMEOUT = int(os.environ.get('OS_TEST_TIMEOUT'))
+    UUID_V4 = uuid.uuid4().hex[:8]
+    AS_GROUP = None
+    AS_CONFIG = None
+    AS_INSTANCE = None
+    MAX_INSTANCES_NUMBER = 1
+    IMAGE_NAME = "Standard_Debian_10_latest"
+    DISK_SIZE = 4
+    KP_NAME = None
+    AS_GROUP_NAME = None
+    network = None
+    keypair = None
 
-    UUID = uuid.uuid4().hex[:9]
-    NETWORK_NAME = "test-as-network-" + UUID
-    SUBNET_NAME = "test-as-subnet-" + UUID
-    ROUTER_NAME = "test-as-router-" + UUID
-    NETWORK = None
-    KEYPAIR = None
-    KP_NAME = "test-as-kp-" + UUID
-    IP_VERSION = 4
-    CIDR = "192.168.0.0/16"
+    def setUp(self):
+        test_timeout = 3 * TestAs.TIMEOUT
+        try:
+            self.useFixture(
+                fixtures.EnvironmentVariable(
+                    'OS_TEST_TIMEOUT', str(test_timeout)))
+        except ValueError:
+            pass
+        super(TestAs, self).setUp()
+        self.client = self.conn.auto_scaling
+        self.client_net = self.conn.network
 
     def create_network(
             self,
             cidr='192.168.0.0/16',
-            ip_version=4
+            ip_version=4,
+            router_name='sdk-as-test-router-',
+            net_name='sdk-as-test-net-',
+            subnet_name='sdk-as-test-subnet-',
+            kp_name='sdk-as-test-kp-'
     ):
-        router_name = self.ROUTER_NAME
-        net_name = self.NETWORK_NAME
-        subnet_name = self.SUBNET_NAME
-        if not BaseASTest.NETWORK:
-            network = self.conn.network.create_network(name=net_name)
+        router_name += TestAs.UUID_V4
+        net_name += TestAs.UUID_V4
+        subnet_name += TestAs.UUID_V4
+        kp_name += TestAs.UUID_V4
+        if not TestAs.network:
+            network = self.client_net.create_network(name=net_name)
             self.assertEqual(net_name, network.name)
             net_id = network.id
-            subnet = self.conn.network.create_subnet(
+            subnet = self.client_net.create_subnet(
                 name=subnet_name,
                 ip_version=ip_version,
                 network_id=net_id,
@@ -54,7 +73,7 @@ class BaseASTest(base.BaseFunctionalTest):
             self.assertEqual(subnet_name, subnet.name)
             subnet_id = subnet.id
 
-            router = self.conn.network.create_router(name=router_name)
+            router = self.client_net.create_router(name=router_name)
             self.assertEqual(router_name, router.name)
             router_id = router.id
             interface = router.add_interface(
@@ -63,60 +82,186 @@ class BaseASTest(base.BaseFunctionalTest):
             )
             self.assertEqual(interface['subnet_id'], subnet_id)
             self.assertIn('port_id', interface)
-            BaseASTest.NETWORK = {
+            TestAs.network = {
                 'router_id': router_id,
                 'subnet_id': subnet_id,
                 'network_id': net_id
             }
-        if not BaseASTest.KEYPAIR:
-            keypair = self.conn.compute.create_keypair(
-                name=self.KP_NAME
-            )
-            self.assertIsNotNone(keypair)
-            BaseASTest.KEYPAIR = keypair
+            if not TestAs.keypair:
+                keypair = self.conn.compute.create_keypair(
+                    name=kp_name
+                )
+                self.assertIsNotNone(keypair)
+                TestAs.keypair = keypair
+                TestAs.KP_NAME = kp_name
         return
 
-    def destroy_network(self):
-        if BaseASTest.NETWORK:
-            params = BaseASTest.NETWORK
-            router_id = params.get('router_id')
-            subnet_id = params.get('subnet_id')
-            network_id = params.get('network_id')
-            router = self.conn.network.get_router(router_id)
+    def destroy_network(self, params: dict, kp_name: str):
+        router_id = params.get('router_id')
+        subnet_id = params.get('subnet_id')
+        network_id = params.get('network_id')
 
-            interface = router.remove_interface(
-                self.conn.network,
-                subnet_id=subnet_id
-            )
-            self.assertEqual(interface['subnet_id'], subnet_id)
-            self.assertIn('port_id', interface)
-            sot = self.conn.network.delete_router(
-                router_id,
-                ignore_missing=False
-            )
-            self.assertIsNone(sot)
-            sot = self.conn.network.delete_subnet(
-                subnet_id,
-                ignore_missing=False
-            )
-            self.assertIsNone(sot)
-            sot = self.conn.network.delete_network(
-                network_id,
-                ignore_missing=False
-            )
-            self.assertIsNone(sot)
-            BaseASTest.NETWORK = None
-            keypair = self.conn.compute.delete_keypair(keypair=self.KP_NAME)
-            self.assertIsNone(keypair)
-            BaseASTest.KEYPAIR = None
+        keypair = self.conn.compute.delete_keypair(keypair=kp_name)
+        self.assertIsNone(keypair)
+        TestAs.keypair = None
 
-    def setUp(self):
-        test_timeout = 3 * int(os.environ.get('OS_TEST_TIMEOUT'))
+        router = self.client_net.get_router(router_id)
+        interface = router.remove_interface(
+            self.conn.network,
+            subnet_id=subnet_id
+        )
+        self.assertEqual(interface['subnet_id'], subnet_id)
+        self.assertIn('port_id', interface)
+        sot = self.client_net.delete_router(
+            router_id,
+            ignore_missing=False
+        )
+        self.assertIsNone(sot)
+        sot = self.client_net.delete_subnet(
+            subnet_id,
+            ignore_missing=False
+        )
+        self.assertIsNone(sot)
+        sot = self.client_net.delete_network(
+            network_id,
+            ignore_missing=False
+        )
+        self.assertIsNone(sot)
+
+    def get_image_id(self):
+        image = self.conn.compute.find_image(
+            name_or_id=self.IMAGE_NAME
+        )
+        if image:
+            return image.id
+
+    def get_default_sec_group(self):
+        sec_group = self.client_net.find_security_group(
+            name_or_id="default"
+        )
+        if sec_group:
+            return sec_group.id
+
+    def create_as_config(self, config_name='sdk-as-test-config-',
+                         disk_vol_type="SATA", flavor="s3.medium.1",
+                         disk_type="SYS", disk_size=4):
+        if not TestAs.AS_CONFIG:
+            config_name += TestAs.UUID_V4
+
+            image_id = self.get_image_id()
+            sec_group_id = self.get_default_sec_group()
+            config_attrs = {
+                "name": config_name,
+                "instance_config": {
+                    "flavorRef": flavor,
+                    "imageRef": image_id,
+                    "disk": [{
+                        'size': disk_size,
+                        'volume_type': disk_vol_type,
+                        'disk_type': disk_type
+                    }],
+                    "key_name": TestAs.KP_NAME,
+                    "security_groups": [{
+                        "id": sec_group_id
+                    }]
+                }
+            }
+            as_config = self.client.create_config(**config_attrs)
+            TestAs.AS_CONFIG = as_config
+
+    def delete_as_config(self):
+        if TestAs.AS_CONFIG:
+            self.client.delete_config(config=TestAs.AS_CONFIG)
+            TestAs.AS_CONFIG = None
+
+    def create_as_group(self, group_name='test-as-group-',
+                        desire_instances_number=1,
+                        min_instances_number=0,
+                        max_instances_number=MAX_INSTANCES_NUMBER):
+        if not TestAs.AS_GROUP:
+            group_name += TestAs.UUID_V4
+            group_attrs = {
+                "scaling_group_name": group_name,
+                "scaling_configuration_id": TestAs.AS_CONFIG.id,
+                "desire_instance_number": desire_instances_number,
+                "min_instance_number": min_instances_number,
+                "max_instance_number": max_instances_number,
+                "vpc_id": TestAs.network.get('router_id'),
+                "networks": [{
+                    "id": TestAs.network.get('network_id')
+                }]
+            }
+            as_group = self.client.create_group(**group_attrs)
+            self.client.resume_group(as_group)
+            as_group = self.client.wait_for_group(as_group)
+            TestAs.AS_GROUP = as_group
+            TestAs.AS_GROUP_NAME = group_name
+            TestAs.MAX_INSTANCES_NUMBER = max_instances_number
+
+    def delete_as_group(self, force_delete=False):
+        if TestAs.AS_GROUP:
+            as_group = TestAs.AS_GROUP
+            self.client.pause_group(as_group)
+            if not force_delete:
+                self.delete_instances(as_group=as_group, timeout=self.TIMEOUT)
+            self.client.delete_group(
+                group=as_group,
+                force_delete=force_delete
+            )
+            self.client.wait_for_delete_group(
+                group=as_group,
+                interval=5,
+                wait=self.TIMEOUT
+            )
+            TestAs.AS_GROUP = None
+
+    def delete_instances(self, as_group, timeout):
         try:
-            self.useFixture(
-                fixtures.EnvironmentVariable(
-                    'OS_TEST_TIMEOUT', str(test_timeout)))
-        except ValueError:
-            pass
-        super(BaseASTest, self).setUp()
-        self.create_network()
+            instances = list(self.client.instances(group=as_group))
+            if instances:
+                for instance in instances:
+                    self.client.remove_instance(
+                        instance=instance,
+                        delete_instance=True
+                    )
+                    self.client.wait_for_delete_instance(
+                        instance=instance, wait=timeout
+                    )
+                return
+        except exceptions.SDKException as e:
+            _logger.warning(
+                'Got exception during getting as instances %s' % e.message
+            )
+
+    def wait_for_instance(self):
+        timeout = int(os.environ.get('OS_TEST_TIMEOUT'))
+        for count in utils.iterate_timeout(
+                timeout=timeout,
+                message="Timeout waiting for instance"
+        ):
+            instances = list(self.client.instances(
+                group=TestAs.AS_GROUP
+            ))
+            if len(instances) == TestAs.MAX_INSTANCES_NUMBER and instances[0].id:
+                return self.client.wait_for_instance(instances[0])
+
+    def delete_instance(self):
+        if TestAs.AS_INSTANCE:
+            self.client.remove_instance(
+                instance=TestAs.AS_INSTANCE,
+                delete_instance=True
+            )
+            self.client.wait_for_delete_instance(
+                instance=TestAs.AS_INSTANCE,
+                wait=self.TIMEOUT
+            )
+
+    def initialize_as_group_with_instance(self):
+        self.create_as_config()
+        self.create_as_group()
+        TestAs.AS_INSTANCE = self.wait_for_instance()
+
+    def deinitialize_as_group_with_instance(self):
+        self.delete_instance()
+        self.delete_as_group()
+        self.delete_as_config()
--- otcextensions/tests/functional/sdk/auto_scaling/v1/test_instance.py
+++ otcextensions/tests/functional/sdk/auto_scaling/v1/test_instance.py
@@ -10,196 +10,52 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import os
-import uuid
-
-from openstack import exceptions
 from openstack import _log
-from openstack import utils
 
-from otcextensions.tests.functional.sdk.auto_scaling.v1 import base
+from otcextensions.tests.functional.sdk.auto_scaling.v1.base import TestAs
 
 _logger = _log.setup_logging('openstack')
 
 
-class TestInstance(base.BaseASTest):
-
-    TIMEOUT = int(os.environ.get('OS_TEST_TIMEOUT'))
-    UUID = uuid.uuid4().hex[:9]
-    AS_GROUP_NAME = "test-as-group-" + UUID
-    AS_CONFIG_NAME = "test-as-config-" + UUID
-    AS_GROUP = None
-    AS_CONFIG = None
-    AS_INSTANCE = None
-    DESIRE_INSTANCE_NUMBER = 1
-    MIN_INSTANCE_NUMBER = 0
-    MAX_INSTANCE_NUMBER = 1
-    FLAVOR = "s3.medium.1"
-    IMAGE_NAME = "Standard_Ubuntu_18.04_latest"
-    DISK_SIZE = 4
-    DISK_VOL_TYPE = "SATA"
-    DISK_TYPE = "SYS"
+class TestInstance(TestAs):
 
     def setUp(self):
         super(TestInstance, self).setUp()
-        self._initialize_as_group_with_instance()
+        self.create_network()
+        self.initialize_as_group_with_instance()
 
-    def _get_image_id(self):
-        image = self.conn.compute.find_image(
-            name_or_id=self.IMAGE_NAME
-        )
-        if image:
-            return image.id
-
-    def _get_default_sec_group(self):
-        sec_group = self.conn.network.find_security_group(
-            name_or_id="default"
-        )
-        if sec_group:
-            return sec_group.id
-
-    def _create_as_config(self):
-        if not TestInstance.AS_CONFIG:
-            image_id = self._get_image_id()
-            sec_group_id = self._get_default_sec_group()
-            config_attrs = {
-                "name": self.AS_CONFIG_NAME,
-                "instance_config": {
-                    "flavorRef": self.FLAVOR,
-                    "imageRef": image_id,
-                    "disk": [{
-                        'size': self.DISK_SIZE,
-                        'volume_type': self.DISK_VOL_TYPE,
-                        'disk_type': self.DISK_TYPE
-                    }],
-                    "key_name": self.KP_NAME,
-                    "security_groups": [{
-                        "id": sec_group_id
-                    }]
-                }
-            }
-            as_config = self.conn.auto_scaling.create_config(**config_attrs)
-            TestInstance.AS_CONFIG = as_config
-
-    def _delete_as_config(self):
-        if TestInstance.AS_CONFIG:
-            self.conn.auto_scaling.delete_config(config=TestInstance.AS_CONFIG)
-            TestInstance.AS_CONFIG = None
-
-    def _create_as_group(self):
-        if not TestInstance.AS_GROUP:
-            group_attrs = {
-                "scaling_group_name": self.AS_GROUP_NAME,
-                "scaling_configuration_id": TestInstance.AS_CONFIG.id,
-                "desire_instance_number": self.DESIRE_INSTANCE_NUMBER,
-                "min_instance_number": self.MIN_INSTANCE_NUMBER,
-                "max_instance_number": self.MAX_INSTANCE_NUMBER,
-                "vpc_id": TestInstance.NETWORK.get('router_id'),
-                "networks": [{
-                    "id": TestInstance.NETWORK.get('network_id')
-                }]
-            }
-            as_group = self.conn.auto_scaling.create_group(**group_attrs)
-            self.conn.auto_scaling.resume_group(as_group)
-            as_group = self.conn.auto_scaling.wait_for_group(as_group)
-            TestInstance.AS_GROUP = as_group
-
-    def _delete_as_group(self, force_delete=False):
-        if TestInstance.AS_GROUP:
-            as_group = TestInstance.AS_GROUP
-            self.conn.auto_scaling.pause_group(as_group)
-            if not force_delete:
-                self._delete_instances(as_group=as_group, timeout=self.TIMEOUT)
-            self.conn.auto_scaling.delete_group(
-                group=as_group,
-                force_delete=force_delete
-            )
-            self.conn.auto_scaling.wait_for_delete_group(
-                group=as_group,
-                interval=5,
-                wait=self.TIMEOUT
-            )
-            TestInstance.AS_GROUP = None
-
-    def _delete_instances(self, as_group, timeout):
-        try:
-            instances = list(self.conn.auto_scaling.instances(group=as_group))
-            if instances:
-                for instance in instances:
-                    self.conn.auto_scaling.remove_instance(
-                        instance=instance,
-                        delete_instance=True
-                    )
-                    self.conn.auto_scaling.wait_for_delete_instance(
-                        instance=instance, wait=timeout
-                    )
-                return
-        except exceptions.SDKException as e:
-            _logger.warning(
-                'Got exception during getting as instances %s' % e.message
-            )
-
-    def _wait_for_instance(self):
-        timeout = int(os.environ.get('OS_TEST_TIMEOUT'))
-        for count in utils.iterate_timeout(
-                timeout=timeout,
-                message="Timeout waiting for instance"
-        ):
-            instances = list(self.conn.auto_scaling.instances(
-                group=TestInstance.AS_GROUP
-            ))
-            if len(instances) == self.MAX_INSTANCE_NUMBER and instances[0].id:
-                return self.conn.auto_scaling.wait_for_instance(instances[0])
-
-    def _delete_instance(self):
-        if TestInstance.AS_INSTANCE:
-            self.conn.auto_scaling.remove_instance(
-                instance=TestInstance.AS_INSTANCE,
-                delete_instance=True
-            )
-            self.conn.auto_scaling.wait_for_delete_instance(
-                instance=TestInstance.AS_INSTANCE,
-                wait=self.TIMEOUT
-            )
-
-    def _initialize_as_group_with_instance(self):
-        self._create_as_config()
-        self._create_as_group()
-        TestInstance.AS_INSTANCE = self._wait_for_instance()
-
-    def _deinitialize_as_group_with_instance(self):
-        self._delete_instance()
-        self._delete_as_group()
-        self._delete_as_config()
-
-    def test_01_find_as_group(self):
-        as_group = self.conn.auto_scaling.find_group(
-            name_or_id=self.AS_GROUP_NAME)
+    def test_01_find_as_group_by_name(self):
+        as_group = self.client.find_group(
+            name_or_id=TestAs.AS_GROUP_NAME)
         self.assertIsNotNone(as_group)
         self.assertEqual(as_group.name, self.AS_GROUP_NAME)
 
     def test_02_list_as_groups(self):
-        as_group_list = list(self.conn.auto_scaling.groups())
+        as_group_list = list(self.client.groups())
         self.assertIsNotNone(as_group_list)
 
     def test_03_update_as_group(self):
         new_name = self.AS_GROUP_NAME
-        as_group = self.conn.auto_scaling.update_group(
-            group=TestInstance.AS_GROUP, name=new_name)
+        as_group = self.client.update_group(
+            group=TestAs.AS_GROUP, name=new_name,
+            desire_instance_number=0)
         self.assertIsNotNone(as_group)
         self.assertEqual(as_group.name, new_name)
 
-    def test_04_find_as_group(self):
+    def test_04_find_as_group_by_id(self):
         find_as_group = self.conn.auto_scaling.find_group(
-            name_or_id=TestInstance.AS_GROUP.id
+            name_or_id=TestAs.AS_GROUP.id
         )
-        self.assertEqual(TestInstance.AS_GROUP.id, find_as_group.id)
-        self.assertEqual(TestInstance.AS_GROUP.name, find_as_group.name)
-        self._deinitialize_as_group_with_instance()
+        self.assertEqual(TestAs.AS_GROUP.id, find_as_group.id)
+        self.assertEqual(TestAs.AS_GROUP.name, find_as_group.name)
 
-    def test_05_delete_as_network(self):
-        self.conn.vpc.find_vpc(name_or_id=TestInstance.NETWORK.get('router_id'))
-        self._delete_as_group()
-        as_group = self.conn.auto_scaling.find_group(
-            name_or_id=self.AS_GROUP_NAME)
+        self.deinitialize_as_group_with_instance()
+        as_group = self.client.find_group(
+            name_or_id=find_as_group.name)
         self.assertIsNone(as_group)
+
+        self.destroy_network(TestAs.network, TestAs.KP_NAME)
+        vpc = self.conn.vpc.find_vpc(
+            TestAs.network.get('router_id'),
+            ignore_missing=True)
+        self.assertIsNone(vpc)
