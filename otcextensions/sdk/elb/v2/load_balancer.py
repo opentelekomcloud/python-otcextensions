6594666f2011e8725f65aad7b95a5286f75247f4
Polina-Gubina <polinagubina6@gmail.com>
2022-11-11 16:27:04 +0100

    fix import fo lb

--- otcextensions/sdk/elb/v2/load_balancer.py
+++ otcextensions/sdk/elb/v2/load_balancer.py
@@ -11,10 +11,9 @@
 # under the License.
 
 from openstack import resource
-from openstack.common import tag
 
 
-class LoadBalancer(resource.Resource, tag.TagMixin):
+class LoadBalancer(resource.Resource):
     resource_key = 'loadbalancer'
     resources_key = 'loadbalancers'
     base_path = '/lbaas/loadbalancers'
@@ -30,8 +29,7 @@ class LoadBalancer(resource.Resource, tag.TagMixin):
         'description', 'flavor_id', 'name', 'project_id', 'provider',
         'vip_address', 'vip_network_id', 'vip_port_id', 'vip_subnet_id',
         'vip_qos_policy_id', 'provisioning_status', 'operating_status',
-        'availability_zone', is_admin_state_up='admin_state_up',
-        **tag.TagMixin._tag_query_parameters
+        'availability_zone', is_admin_state_up='admin_state_up'
     )
 
     # Properties
