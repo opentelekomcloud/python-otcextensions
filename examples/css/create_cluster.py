95e822034db8e9c95d76256643ad8d69fe7ce545
Vineet Pruthi <48789821+vineet-pruthi@users.noreply.github.com>
2023-01-19 18:35:32 +0100

    Adding some fixes for CSS (#316)
    Adding some fixes for CSS
    
    Reviewed-by: Vladimir Vshivkov
    Reviewed-by: Anton Sidelnikov

--- examples/css/create_cluster.py
+++ examples/css/create_cluster.py
@@ -14,20 +14,20 @@
 Create CSS Cluster
 """
 import openstack
-from otcextensions import sdk
 
 openstack.enable_logging(True)
-conn = openstack.connect(cloud='otc-test')
-sdk.register_otc_extensions(conn)
+conn = openstack.connect(cloud='otc')
+
 
 attrs = {
     'name': 'ES-Test',
-    'instanceNum': 1,
+    'instanceNum': 3,
     'datastore': {
         'type': 'elasticsearch',
         'version': '7.6.2'
     },
     'instance': {
+        'availability_zone': 'eu-de-01',
         'flavorRef': 'css.xlarge.2',
         'volume': {
             'volume_type': 'COMMON',
