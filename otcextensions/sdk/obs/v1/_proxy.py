05bebd14457fb6419d955d97c803a5f7cada3290
anton-sidelnikov <a.sidelnikov@t-systems.com>
2023-09-21 15:09:08 +0200

    fixes for support other regions

--- otcextensions/sdk/obs/v1/_proxy.py
+++ otcextensions/sdk/obs/v1/_proxy.py
@@ -11,11 +11,13 @@
 # under the License.
 import os
 from urllib import parse
+from urllib.parse import urlsplit
 
 from openstack import exceptions
 from urllib3.exceptions import LocationParseError
 
 from otcextensions.common import utils
+from otcextensions.common.utils import extract_region_from_url
 from otcextensions.sdk import ak_auth
 from otcextensions.sdk import sdk_proxy
 from otcextensions.sdk.obs.v1 import container as _container
@@ -60,15 +62,9 @@ class Proxy(sdk_proxy.Proxy):
         """Override to return mapped endpoint if override and region are set
 
         """
-        region_name = getattr(self, 'region_name', 'eu-de')
-        if region_name == 'eu-ch2':
-            self.CONTAINER_ENDPOINT = \
-                'https://%(container)s.obs.%(region_name)s.sc.otc.t-systems.com'
-        endpoint = self.CONTAINER_ENDPOINT % {
-            'container': container,
-            'region_name': region_name
-        }
-        return endpoint
+        split_url = urlsplit(self.get_endpoint())
+
+        return f'{split_url.scheme}://%(container)s.{split_url.netloc}' % {'container': container}
 
     def _get_req_auth(self, host=None):
         auth = getattr(self, '_ak_auth', None)
@@ -81,7 +77,7 @@ class Proxy(sdk_proxy.Proxy):
             if not (ak and sk):
                 self.log.error('Cannot obtain AK/SK from config')
                 return None
-            region = getattr(self, 'region_name', 'eu-de')
+            region = extract_region_from_url(self.get_endpoint())
             if not host:
                 host = self.get_endpoint()
             auth = ak_auth.AKRequestsAuth(
