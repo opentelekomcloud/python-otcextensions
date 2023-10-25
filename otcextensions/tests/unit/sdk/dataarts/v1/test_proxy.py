5031dc260e95298ccbc8ad15bab105106d120805
mmazec <milan_mazec@hotmail.com>
2023-10-25 08:49:02 +0000

    added proxy class for dataarts service

---
+++ otcextensions/tests/unit/sdk/dataarts/v1/test_proxy.py
@@ -0,0 +1,29 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+
+from otcextensions.sdk.dataarts.v1 import _proxy
+from openstack.tests.unit import test_proxy_base
+
+
+ENDPOINT_DATAARTS = 'http://dataarts.example.com/v1.0'
+
+
+class TestDataartsProxy(test_proxy_base.TestProxyBase):
+
+    def setUp(self):
+        super(TestDataartsProxy, self).setUp()
+        self.proxy = _proxy.Proxy(self.session)
+        self.session.get_endpoint = mock.Mock(
+            return_value=ENDPOINT_DATAARTS
+        )
