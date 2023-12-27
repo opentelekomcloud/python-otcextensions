4d5a1b095dc7b26757b601295ad5bbae127b8157
Polina Gubina <33940358+Polina-Gubina@users.noreply.github.com>
2023-12-27 11:37:36 +0100

    Lts (#394)
    Lts
    
    Reviewed-by: Anton Sidelnikov
    Reviewed-by: YustinaKvr

--- otcextensions/sdk/lts/v2/stream.py
+++ otcextensions/sdk/lts/v2/stream.py
@@ -10,6 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from openstack import exceptions
 from openstack import resource
 
 
@@ -42,3 +43,72 @@ class Stream(resource.Resource):
     filter_count = resource.Body('filter_count', type=int)
     #: Log stream tag.
     tag = resource.Body('tag')
+
+    @classmethod
+    def list(
+        cls,
+        session,
+        paginated=True,
+        base_path=None,
+        allow_unknown_params=False,
+        *,
+        microversion=None,
+        **params,
+    ):
+        session = cls._get_session(session)
+
+        if microversion is None:
+            microversion = cls._get_microversion(session, action='list')
+
+        if base_path is None:
+            base_path = cls.base_path
+
+        api_filters = cls._query_mapping._validate(
+            params,
+            base_path=base_path,
+            allow_unknown_params=True,
+        )
+        query_params = cls._query_mapping._transpose(api_filters, cls)
+        uri = base_path % params
+        uri_params = {}
+
+        while uri:
+            # Copy query_params due to weird mock unittest interactions
+            response = session.get(
+                uri,
+                headers={"Accept": "application/json"},
+                params=query_params.copy(),
+                microversion=microversion,
+            )
+            exceptions.raise_from_response(response)
+            data = response.json()
+
+            query_params.pop('marker', None)
+            query_params.pop('limit', None)
+
+            if cls.resources_key:
+                resources = data[cls.resources_key]
+            else:
+                resources = data
+
+            if not isinstance(resources, list):
+                resources = [resources]
+
+            for raw_resource in resources:
+                # Do not allow keys called "self" through. Glance chose
+                # to name a key "self", so we need to pop it out because
+                # we can't send it through cls.existing and into the
+                # Resource initializer. "self" is already the first
+                # argument and is practically a reserved word.
+                raw_resource.pop("self", None)
+                # We want that URI props are available on the resource
+                raw_resource.update(uri_params)
+
+                value = cls.existing(
+                    microversion=microversion,
+                    connection=session._get_connection(),
+                    **raw_resource,
+                )
+                yield value
+
+            return
