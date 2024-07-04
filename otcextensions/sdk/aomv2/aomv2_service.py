2c59e378ca14be08dcb9262405c800b8971f36fa
Valeriia Ziukina <sand16031986@gmail.com>
2024-07-04 10:29:26 +0200

    empty proxy ready

--- otcextensions/sdk/aomv2/aomv2_service.py
+++ otcextensions/sdk/aomv2/aomv2_service.py
@@ -9,9 +9,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-import warnings
 
-from openstack import exceptions
 from openstack import service_description
 
 from otcextensions.sdk.imsv2.v2 import _proxy
