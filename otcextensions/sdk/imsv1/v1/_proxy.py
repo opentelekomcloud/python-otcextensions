63a42237724ff3bf1eb8dbed23dcb99708671c92
Josef Wuebbels <josef.wuebbels@t-systems.com>
2024-07-18 11:52:15 +0000

    Add wait_for_async_job method for imsv1

--- otcextensions/sdk/imsv1/v1/_proxy.py
+++ otcextensions/sdk/imsv1/v1/_proxy.py
@@ -9,7 +9,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-from openstack import proxy
+from openstack import proxy, utils
 from otcextensions.sdk.imsv1.v1 import async_job as _async_job
 from otcextensions.sdk.imsv1.v1 import image_export as _image_export
 
@@ -30,6 +30,32 @@ class Proxy(proxy.Proxy):
             project_id=self.session.get_project_id()
         )
         return self._get(_async_job.AsyncJob, base_path=base_path, **attrs)
+
+    def wait_for_async_job(self, job_id, timeout=600,
+                           message="Timeout waiting for asynchronous job.",
+                           wait=10):
+        """Wait for an asynchronous job to finish
+
+        :returns: One
+             :class:`~otcextensions.sdk.imsv2.v1.async_job.AsyncJob`
+        """
+        # Maybe someone provided an object instead of a job_id
+        # try to get the job id from that object if possible
+        if hasattr(job_id, 'job_id'):
+            job_id = job_id.job_id
+
+        for count in utils.iterate_timeout(
+                timeout=timeout,
+                message=message,
+                wait=wait):
+            job = self.get_async_job(job_id)
+            if job.status == "FAIL":
+                raise RuntimeError(
+                    "Asynchronous job {job_id} failed.".format(job_id=job_id)
+                )
+            elif not job.status in ['INIT', 'RUNNING']:
+                break
+        return job
 
     def export_image(self, **attrs):
         """Export an image
