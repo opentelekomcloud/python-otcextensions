17a4a7babe0e3a805a5a33f53287015263d6e932
Sebastian Gode <sebastian.gode@telekom.de>
2025-02-12 12:17:10 +0000

    Image update

--- otcextensions/sdk/imsv2/v2/image.py
+++ otcextensions/sdk/imsv2/v2/image.py
@@ -12,6 +12,8 @@
 # under the License.
 
 from openstack import resource
+from openstack import utils
+from openstack import exceptions
 
 
 class Image(resource.Resource):
@@ -21,9 +23,12 @@ class Image(resource.Resource):
 
     allow_create = True
     allow_list = True
+    allow_delete = True
+    allow_commit = True
 
     #: Method for creating a resource (POST, PUT)
     create_method = "POST"
+    commit_method = "PATCH"
 
     _query_mapping = resource.QueryParameters('limit', 'id',
                                               'name', 'status',
@@ -59,9 +64,9 @@ class Image(resource.Resource):
                                               'enterprise_project_id')
 
     name = resource.Body('name')
-    description = resource.Body('description')
+    description = resource.Body('__description')
     os_type = resource.Body('os_type')
-    os_version = resource.Body('os_version')
+    os_version = resource.Body('__os_version')
     image_url = resource.Body('image_url')
     instance_id = resource.Body('instance_id')
     min_disk = resource.Body('min_disk', type=int)
@@ -85,8 +90,53 @@ class Image(resource.Resource):
     virtual_env_type = resource.Body('virtual_env_type')
     enterprise_project_id = resource.Body('enterprise_project_id')
     protected = resource.Body('protected', type=bool)
+    virtual_size = resource.Body('virtual_size')
+    hw_firmware_type = resource.Body('hw_firmware_type')
+    disk_format = resource.Body('disk_format')
+    container_format = resource.Body('container_format')
+    hw_vif_multiqueue_enabled = resource.Body('hw_vif_multiqueue_enabled')
+    checksum = resource.Body('checksum')
+    size = resource.Body('size')
+    file = resource.Body('file')
+    os_bit = resource.Body('__os_bit')
+    platform = resource.Body('__platform')
+    is_registered = resource.Body('__is_registered')
+    os_type = resource.Body('__os_type')
+    image_source_type = resource.Body('__image_source_type')
+    imagetype = resource.Body('__imagetype')
+    originalimagename = resource.Body('__originalimagename')
+    backup_id = resource.Body('__backup_id')
+    productcode = resource.Body('__productcode')
+    image_size = resource.Body('__image_size')
+    support_fc_inject = resource.Body('__support_fc_inject')
+    data_origin = resource.Body('__data_origin')
+
 
     def create(self, session, prepend_key=False, base_path=None):
         # Overriden here to override prepend_key default value
         return super(Image, self).create(session, prepend_key, base_path
                                          + '/action')
+    
+    def _action(self, session, request_body, image_id):
+        url = utils.urljoin(self.base_path, image_id)
+        response = session.patch(url, json=request_body)
+        exceptions.raise_from_response(response)
+        return response
+
+    def update_image_details(self, session, image_id, command_list):
+        request_body = command_list
+        response = self._action(session=session, request_body=request_body, image_id=image_id)
+        return self._to_object(session, response)     
+
+    def _to_object(self, session, response):
+        has_body = (
+            self.has_body
+            if self.create_returns_body is None
+            else self.create_returns_body
+        )
+        microversion = self._get_microversion(session, action='create')
+        self.microversion = microversion
+        self._translate_response(response, has_body=has_body)
+        if self.has_body and self.create_returns_body is False:
+            return self.fetch(session)
+        return self
