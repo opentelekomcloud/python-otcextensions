834d403bf382109b6e0e7a2c7b8095533e2f542c
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2025-02-18 19:03:59 +0100

    [Feat.] APIG environment variables (#535)
    [Feat.] APIG environment variables
    
    Implements: https://docs.otc.t-systems.com/api-gateway/api-ref/dedicated_gateway_apis_v2/environment_variable_management/index.html
    
    Reviewed-by: Muneeb H. Jan <muneebhafeezjan@gmail.com>
    Reviewed-by: Artem Lifshits

---
+++ otcextensions/tests/functional/sdk/apig/v2/test_environment_var.py
@@ -0,0 +1,91 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from otcextensions.tests.functional.sdk.apig import TestApiG
+
+
+class TestEnvironmentVar(TestApiG):
+    environment = None
+
+    def setUp(self):
+        super(TestEnvironmentVar, self).setUp()
+        self.create_gateway()
+        self.gateway_id = TestEnvironmentVar.gateway.id
+        # self.gateway_id = "be76ca6de5fe4aa7af503c03b3b44dea"
+        env_attrs = {
+            "name": "DEV",
+            "remark": "Development environment"
+        }
+        environment = self.client.create_environment(
+            gateway=self.gateway_id,
+            **env_attrs)
+        self.assertIsNotNone(environment.id)
+
+        group_attrs = {
+            "name": "api_group_001",
+            "remark": "API group 1"
+        }
+        self.group = self.client.create_api_group(
+            gateway=self.gateway_id,
+            **group_attrs
+        )
+        self.assertIsNotNone(self.group.id)
+
+        self.attrs = {
+            "variable_name": "address",
+            "variable_value": "192.168.1.5",
+            "env_id": environment.id,
+            "group_id": self.group.id
+        }
+        self.variable = self.client.create_environment_variable(
+            gateway=self.gateway_id,
+            **self.attrs
+        )
+
+        self.addCleanup(
+            self.client.delete_environment_variable,
+            gateway=self.gateway_id,
+            var=self.variable
+        )
+        self.addCleanup(
+            self.client.delete_environment,
+            gateway=self.gateway_id,
+            environment=environment.id,
+        )
+        self.addCleanup(
+            self.client.delete_api_group,
+            gateway=self.gateway_id,
+            api_group=self.group.id,
+        )
+
+    def test_list_environment_variables(self):
+        vars = list(self.client.environment_variables(
+            gateway=self.gateway_id,
+            group_id=self.group.id))
+        self.assertEqual(len(vars), 1)
+
+    def test_get_environment_variable(self):
+        var = self.client.get_environment_variable(
+            gateway=self.gateway_id,
+            var=self.variable.id)
+        self.assertEqual(var.id, self.variable.id)
+
+    def test_update_environment_variable(self):
+        attrs = {
+            "variable_value": "192.168.1.6",
+        }
+        updated = self.client.update_environment_variable(
+            gateway=self.gateway_id,
+            var=self.variable.id,
+            **attrs
+        )
+        self.assertEqual(updated.variable_value, attrs["variable_value"])
---
+++ otcextensions/tests/unit/sdk/apig/v2/test_env_var.py
@@ -0,0 +1,44 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+from openstack.tests.unit import base
+from otcextensions.sdk.apig.v2 import apienvironmentvar as var
+
+EXAMPLE_VAR = {
+    "variable_value": "192.168.1.5",
+    "env_id": "7a1ad0c350844ee69479b47df9a881cb",
+    "group_id": "c77f5e81d9cb4424bf704ef2b0ac7600",
+    "id": "25054838a624400bbf2267cf5b3a3f70",
+    "variable_name": "address"
+}
+
+
+class TestApiEnvironment(base.TestCase):
+
+    def test_basic(self):
+        sot = var.ApiEnvironmentVar()
+        self.assertEqual(
+            '/apigw/instances/%(gateway_id)s/env-variables',
+            sot.base_path)
+        self.assertTrue(sot.allow_list)
+        self.assertTrue(sot.allow_create)
+        self.assertTrue(sot.allow_commit)
+        self.assertTrue(sot.allow_delete)
+        self.assertTrue(sot.allow_fetch)
+        self.assertEqual('variables', sot.resources_key)
+
+    def test_make_it(self):
+        sot = var.ApiEnvironmentVar(**EXAMPLE_VAR)
+        self.assertEqual(EXAMPLE_VAR['variable_value'], sot.variable_value)
+        self.assertEqual(EXAMPLE_VAR['env_id'], sot.env_id)
+        self.assertEqual(EXAMPLE_VAR['group_id'], sot.group_id)
+        self.assertEqual(EXAMPLE_VAR['id'], sot.id)
+        self.assertEqual(EXAMPLE_VAR['variable_name'], sot.variable_name)
