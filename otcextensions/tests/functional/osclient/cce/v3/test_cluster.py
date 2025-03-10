5657207075b7c8563011e025b4f44cb79207b54e
anton-sidelnikov <a.sidelnikov@t-systems.com>
2025-03-10 12:13:56 +0100

    [Fix] cce more checks for failing test

--- otcextensions/tests/functional/osclient/cce/v3/test_cluster.py
+++ otcextensions/tests/functional/osclient/cce/v3/test_cluster.py
@@ -19,10 +19,10 @@ from openstackclient.tests.functional import base
 class TestCce(base.TestCase):
     """Functional tests for CCE Instance. """
 
-    UUID = uuid.uuid4().hex[:8]
-    ROUTER_NAME = 'sdk-test-router-' + UUID
-    NET_NAME = 'sdk-test-net-' + UUID
-    SUBNET_NAME = 'sdk-test-subnet-' + UUID
+    UUID = uuid.uuid4().hex[:4]
+    ROUTER_NAME = 'sdk--cce-test-router-' + UUID
+    NET_NAME = 'sdk-cce-test-net-' + UUID
+    SUBNET_NAME = 'sdk-cce-test-subnet-' + UUID
     ROUTER_ID = None
     NET_ID = None
 
@@ -36,17 +36,13 @@ class TestCce(base.TestCase):
     def test_02_cluster_create(self):
         self._initialize_network()
         cmd = (
-            'cce cluster create '
-            '{name} {router} {network} '
-            '--description descr '
-            '--flavor cce.s1.small '
-            '--container-network-mode overlay_l2 '
-            '--wait --wait-interval 10 '
-            '-f json'.format(
-                name=self.CLUSTER_NAME,
-                router=self.ROUTER_NAME,
-                network=self.NET_NAME
-            )
+            f'cce cluster create '
+            f'{self.CLUSTER_NAME} {self.ROUTER_NAME} {self.NET_NAME} '
+            f'--description descr '
+            f'--flavor cce.s1.small '
+            f'--container-network-mode overlay_l2 '
+            f'--wait --wait-interval 10 '
+            f'-f json'
         )
 
         json_output = json.loads(self.openstack(cmd))
@@ -56,34 +52,29 @@ class TestCce(base.TestCase):
     def test_03_cluster_delete(self):
         self.addCleanup(self._deinitialize_network)
         self.openstack(
-            'cce cluster delete {name} '
-            '--wait --wait-interval 10'.format(
-                name=self.CLUSTER_NAME
-            )
+            f'cce cluster delete {self.CLUSTER_NAME} --wait --wait-interval 10'
         )
 
     def _initialize_network(self):
         router = json.loads(self.openstack(
             'router create -f json ' + self.ROUTER_NAME
         ))
+        self.assertIsNotNone(router)
+
         net = json.loads(self.openstack(
             'network create -f json ' + self.NET_NAME
         ))
-        self.openstack(
-            'subnet create {subnet} -f json '
-            '--network {net} '
-            '--subnet-range 192.168.0.0/24 '.format(
-                subnet=self.SUBNET_NAME,
-                net=self.NET_NAME
-            )
-        )
+        self.assertIsNotNone(net)
+
+        subnet = json.loads(self.openstack(
+            f'subnet create {self.SUBNET_NAME} '
+            f'-f json --network {self.NET_NAME} '
+            f'--subnet-range 192.168.0.0/24'
+        ))
+        self.assertIsNotNone(subnet)
 
         self.openstack(
-            'router add subnet {router} '
-            '{subnet} '.format(
-                router=self.ROUTER_NAME,
-                subnet=self.SUBNET_NAME
-            )
+            f'router add subnet {self.ROUTER_NAME} {self.SUBNET_NAME}'
         )
 
         self.ROUTER_ID = router['id']
@@ -91,18 +82,8 @@ class TestCce(base.TestCase):
 
     def _deinitialize_network(self):
         self.openstack(
-            'router remove subnet {router} '
-            '{subnet} '.format(
-                router=self.ROUTER_NAME,
-                subnet=self.SUBNET_NAME
+            f'router remove subnet {self.ROUTER_NAME} {self.SUBNET_NAME}'
             )
-        )
-        self.openstack(
-            'subnet delete ' + self.SUBNET_NAME
-        )
-        self.openstack(
-            'network delete ' + self.NET_NAME
-        )
-        self.openstack(
-            'router delete ' + self.ROUTER_NAME
-        )
+        self.openstack('subnet delete ' + self.SUBNET_NAME)
+        self.openstack('network delete ' + self.NET_NAME)
+        self.openstack('router delete ' + self.ROUTER_NAME)
