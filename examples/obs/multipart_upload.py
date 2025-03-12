e33451629399d7d87f9d07f5416048371a2da924
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2025-03-12 15:41:05 +0100

    [Feat.] now name and filename attr is supported in multypart upload (#556)
    [Feat.] now name and filename attr is supported in multypart upload
    
    Closes #548
    in case that you need to write different from provided filename name to your object then need to pass name and filename:
    obj = conn.obs.upload_object(
        name='pdfs/book.pdf',
        filename='/home/linux/myfile.zip',
        container=container,
        segment_size=1400000
    )
    
    will create object with book.pdf name inside pdfs folder
    if you want to use only filename then use:
    obj = conn.obs.upload_object(
        name='/home/linux/myfile.zip',
        container=container,
        segment_size=1400000
    )
    
    will create object with myfile.zip name
    
    Reviewed-by: Aloento
    Reviewed-by: Muneeb H. Jan <muneebhafeezjan@gmail.com>

--- examples/obs/multipart_upload.py
+++ examples/obs/multipart_upload.py
@@ -27,7 +27,8 @@ container = conn.obs.create_container(
 )
 
 obj = conn.obs.upload_object(
-    name='myfile.zip',
+    name='pdfs/book.pdf',
+    filename='my-book.pdf',
     container=container,
     segment_size=1400000
 )
