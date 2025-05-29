37060bc40967559a77023666630da7a914a6ac7d
anton-sidelnikov <a.sidelnikov@t-systems.com>
2025-05-29 16:25:17 +0200

    [Fix] quota type rm

--- otcextensions/tests/functional/sdk/auto_scaling/v1/test_quota.py
+++ otcextensions/tests/functional/sdk/auto_scaling/v1/test_quota.py
@@ -26,8 +26,7 @@ class TestQuota(base.TestAs):
     def test_list(self):
         expected_types = ['scaling_Group', 'scaling_Config',
                           'scaling_Policy', 'scaling_Instance',
-                          'load_balance', 'diagnostic_report',
-                          'bandwidth_scaling_policy']
+                          'load_balance', 'bandwidth_scaling_policy']
         objects = list(self.auto_scaling.quotas())
 
         self.assertGreaterEqual(len(objects), 6)
