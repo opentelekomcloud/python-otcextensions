30b78d24697a4f14f681e8667df99691b373d47a
Shushanik Hovhannesyan <shhovhan@gmail.com>
2026-05-15 15:03:01 +0200

    Fix endpoint service type

--- otcextensions/sdk/__init__.py
+++ otcextensions/sdk/__init__.py
@@ -73,7 +73,7 @@ OTC_SERVICES = {
     "ccn": {
         "service_type": "ccn",
         "append_project_id": True,
-        "endpoint_service_type": "ccn",
+        "endpoint_service_type": "cc",
     },
     "ces": {
         "service_type": "ces",
